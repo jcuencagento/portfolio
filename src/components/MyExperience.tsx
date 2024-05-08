70ec23db6a881bbfcb6d3b346d4f3696283ecba1
Javi Cuenca <jcuencagento@gmail.com>
2024-05-08 17:20:17 +0200

    Try some UI CSS stuff

--- src/components/MyExperience.tsx
+++ src/components/MyExperience.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable react/button-has-type */
+import styles from "@/styles/myexperience.module.css";
+
 export default function MyExperience() {
     return (
         <div className="flex w-full flex-col gap-4">
@@ -28,6 +32,25 @@ export default function MyExperience() {
                         </article>
                     </div>
                 </div>
+                <article className={styles.container}>
+                    <div className={styles.terminal_toolbar}>
+                        <div className={styles.butt}>
+                            <button className={styles.btn_red} />
+                            <button className={styles.btn} />
+                            <button className={styles.btn} />
+                        </div>
+                        <p className={styles.user}>jcuencagento@admin: ~</p>
+                        <div className={styles.add_tab}>+</div>
+                    </div>
+                    <div className={styles.terminal_body}>
+                        <div className={styles.terminal_promt}>
+                            <span className={styles.terminal_user}>jcuencagento@admin:</span>
+                            <span className={styles.terminal_location}>~</span>
+                            <span className={styles.terminal_bling}>$</span>
+                            <span className={styles.terminal_cursor} />
+                        </div>
+                    </div>
+                </article>
             </article>
         </div>
     );
--- src/components/MyStudies.tsx
+++ src/components/MyStudies.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable react/button-has-type */
+import styles from "@/styles/myexperience.module.css";
+
 export default function MyStudies() {
     return (
         <div className="flex w-full flex-col gap-4">
@@ -20,6 +24,20 @@ export default function MyStudies() {
                 className="relative m-auto flex h-full w-full transform-gpu items-center justify-center rounded-2xl border-4 border-primary shadow-xl lg:p-8"
             >
                 <div className="align-center flex h-full w-full flex-col justify-center p-4 py-28">
+                    <article className={styles.toggle_container}>
+                        <input className={styles.toggle_input} type="checkbox" />
+                        <div className={styles.toggle_handle_wrapper}>
+                            <div className={styles.toggle_handle}>
+                                <div className={styles.toggle_handle_knob} />
+                                <div className={styles.toggle_handle_bar_wrapper}>
+                                    <div className={styles.toggle_handle_bar} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.toggle_base}>
+                            <div className={styles.toggle_base_inside} />
+                        </div>
+                    </article>
                     <div className="transform-gpu duration-300 ease-in-out">
                         <article className="m-auto flex flex-col">
                             <p className="text-base font-extrabold opacity-100 lg:text-lg">Technology Bachelors and ESO</p>
